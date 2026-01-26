d4878318b1e1d2cafdb6e95aa0fe1aa73c2d62b6
Stephie-Grafika <164401918+Stephie-Grafika@users.noreply.github.com>
2026-01-26 22:08:55 +0100

    Mutation Algorithm Beispiele

--- script-28-factorial-calculator.js
+++ script-28-factorial-calculator.js
@@ -1,13 +1,18 @@
 function mutationAlgorithmStarten(){
     function mutation(arr){
-    let word1 = arr[0].toLowerCase();
-    let word2 = arr[1].toLowerCase();
+        let word1 = arr[0].toLowerCase();
+        let word2 = arr[1].toLowerCase();
 
-    for (const char of word2) {
-        if (!word1.includes(char)){
-        return false;
+        for (const char of word2) {
+            if (!word1.includes(char)){
+            return false;
+            }
         }
+        return true;
     }
-    return true;
-}
+    /* 
+    mutation(["hello", "Hello"]) = true
+    mutation(["hello", "hey"]) = false
+    mutation(["Alien", "line"]) = true
+    */
 }
